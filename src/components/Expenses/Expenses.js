c2b1d7275260ab3b0f85249f77cb3e842888fc7d
Harishkumar J <harishwells@gmail.com>
2022-08-19 23:50:10 +0530

    Rendering lists of data(using Map)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,32 +13,21 @@ const Expenses = (props) => {
 	};
 	console.log('state = ' + filteredYear);
 	return (
-		<Card className='expenses'>
-			<ExpenseFilter
-				defaultYear={filteredYear}
-				onYearFilter={filterByYearHandler}
-			/>
-			<ExpenseItem
-				title={props.items[0].title}
-				amount={props.items[0].amount}
-				date={props.items[0].date}
-			/>
-			<ExpenseItem
-				title={props.items[1].title}
-				amount={props.items[1].amount}
-				date={props.items[1].date}
-			/>
-			<ExpenseItem
-				title={props.items[2].title}
-				amount={props.items[2].amount}
-				date={props.items[2].date}
-			/>
-			<ExpenseItem
-				title={props.items[3].title}
-				amount={props.items[3].amount}
-				date={props.items[3].date}
-			/>
-		</Card>
+		<>
+			<Card className='expenses'>
+				<ExpenseFilter
+					defaultYear={filteredYear}
+					onYearFilter={filterByYearHandler}
+				/>
+				{props.items.map((expenseItem) => (
+					<ExpenseItem
+						title={expenseItem.title}
+						amount={expenseItem.amount}
+						date={expenseItem.date}
+					/>
+				))}
+			</Card>
+		</>
 	);
 };
 
